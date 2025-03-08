cc0e83af7e626ea4ff678cea6b4fe73bd6413c88
jigyasatnwrr <jigyasa.24564@sscbs.du.ac.in>
2025-03-07 23:23:25 -0800

    fake urgency

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-
+let count = 50; // Starting count
+    let interval = setInterval(() => {
+        if(count > 3) {
+            count--;
+            document.getElementById('counter').innerText = `Only ${count} left!`;
+        } 
+        if(count === 3) {
+            document.getElementById('counter').innerText = `Only 3 left! Hurry up!`;
+            document.getElementById('message').innerText = `Don't miss out! This offer is almost gone.`;
+            clearInterval(interval);
+        }
+    }, 60000); 
   
   
